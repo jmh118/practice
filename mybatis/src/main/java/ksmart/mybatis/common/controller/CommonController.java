1278e55e9b20c908295c331eedcaec5a7c7e126e
jmh118 <nnyzip@gmail.com>
2024-10-14 23:01:36 +0900

    common 기능 완성!!

--- mybatis/src/main/java/ksmart/mybatis/common/controller/CommonController.java
+++ mybatis/src/main/java/ksmart/mybatis/common/controller/CommonController.java
@@ -5,5 +5,9 @@ public class CommonController {
 	public void common() {
 		System.out.println("집에서 작업함!!!");
 	}
+	
+	public void complete() {
+		System.out.println("작업 끝남 하구언에서!! develop이랑 합쳐야함!!");
+	}
 
 }
